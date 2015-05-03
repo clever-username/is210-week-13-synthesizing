db960cb8ad4744b3697051dec6fdda88a30fd8ff
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2015-05-03 00:54:37 +0000

    Added required docstrings

--- picklecache.py
+++ picklecache.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 01 module"""
+"""This program opens, reads, and writes to a file."""
 
 import os
 import pickle
 
 
 class PickleCache(object):
-    """Class object docstring.
+    """Class object to manage file.
 
     """
 
     def __init__(self, filepath='datastore.pkl', autosync=False):
-        """Constructor.
+        """Constructor for PickleCache.
 
         Args:
-        filepath(object): Accepts filename as argument.
-        autosync(bool): Accepts boolean as argument.
+            filepath(object): Accepts filename as argument.
+            autosync(bool): Accepts boolean as argument.
+
+        Example:
+            >>> cacher = PickleCache()
+            >>> print cacher._PickleCache__file_path
+            'datastore.pkl'
+            >>> print cacher._PickleCache__data
+            {}
 
         """
         self.__file_path = filepath
@@ -25,17 +32,40 @@ class PickleCache(object):
         self.load()
 
     def __setitem__(self, key, value):
-        """Docstring.
+        """This function sets key-value pairs for storing.
+
+        Args:
+            key(string): A required argument.
+            value(string): A required argument.
+
+        Example:
+            >>> pcache = PickleCache()
+            >>> pcache['test'] = 'hello'
+            >>> print pcache._PickleCache__data['test']
+            'hello'
         """
         self.__data[key] = value
 
     def __len__(self):
-        """Docstring.
+        """This function returns the length of __data.
+
+        Example:
+            >>> len(pcache)
+            1
         """
         return len(self.__data)
         
     def __getitem__(self, key):
-        """Docstring.
+        """This function gets values from __data.
+
+        Args:
+            key(str): A required input.
+
+        Example:
+            >>> pcache = PickleCache()
+            >>> pcache['test'] = 'hello'
+            >>> print pcache['test']
+            'hello'
         """
         try:
             self.__data[key]
@@ -44,12 +74,24 @@ class PickleCache(object):
             raise Exception
 
     def __delitem__(self, key):
-        """Docstring.
+        """This function deletes values from __data.
+
+        Args:
+            key(str): A required argument.
+
+        Example:
+            >>> pcache = PickleCache()
+            >>> pcache['test'] = 'hello'
+            >>> print len(pcache)
+            1
+            >>> del pcache['test']
+            >>> print len(pcache)
+            0
         """
         del self.__data[key]
 
     def load(self):
-        """Docstring.
+        """This function loads a file provided it exists and > 0 byte.
         """
         if os.path.exists(self.__file_path) and \
         os.path.getsize(self.__file_path) > 0:
@@ -58,7 +100,7 @@ class PickleCache(object):
             myfile.close()
 
     def flush(self):
-        """Docstring.
+        """This function writes to and closes the file.
         """
         myfile = open(self.__file_path, 'w')
         pickle.dump(self.__data, myfile)
